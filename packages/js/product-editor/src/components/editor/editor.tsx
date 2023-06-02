9ddb8b177b17cdb6dc91571c63f10cd1582d349d
Matt Sherman <matt@jam123.com>
2023-06-02 16:39:45 -0400

    Add Footer to interface skeleton

--- packages/js/product-editor/src/components/editor/editor.tsx
+++ packages/js/product-editor/src/components/editor/editor.tsx
@@ -34,6 +34,7 @@ import { FullscreenMode, InterfaceSkeleton } from '@wordpress/interface';
 /**
  * Internal dependencies
  */
+import { Footer } from '../footer';
 import { Header } from '../header';
 import { BlockEditor } from '../block-editor';
 import { ValidationProvider } from '../../contexts/validation-context';
@@ -85,6 +86,9 @@ export function Editor( { product, settings }: EditorProps ) {
 											{ /* @ts-expect-error 'scope' does exist. @types/wordpress__plugins is outdated. */ }
 											<PluginArea scope="woocommerce-product-block-editor" />
 										</>
+									}
+									footer={
+										<Footer productId={ product.id } />
 									}
 								/>
 
